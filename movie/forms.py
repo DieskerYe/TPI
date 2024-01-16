eda7c53d652721cf0d422f090844b3243aceb9ff
DieskerYe <matiasdieguez19@gmail.com>
2024-01-15 21:44:46 -0300

    Added comments app with models and forms. Added comments to the movie review template

--- movie/forms.py
+++ movie/forms.py
@@ -3,7 +3,7 @@ from movie.models import Review, RATE_OPCIONES
 
 
 class RateForm(forms.ModelForm):
-    text = forms.CharField(widget=forms.Textarea(attrs={'class': 'materialize-texarea'}), required=True)
+    text = forms.CharField(widget=forms.Textarea(attrs={'class': 'materialize-textarea'}), required=True)
     rate = forms.ChoiceField(choices=RATE_OPCIONES, widget=forms.Select(), required=True)
 
     class Meta:
