b3ee241a6377742e47e4cb268e49cecd19ffc7fb
DieskerYe <matiasdieguez19@gmail.com>
2024-01-17 01:05:00 -0300

    Added login and dropdown to the index

--- TPI/static/js/init.js
+++ TPI/static/js/init.js
@@ -4,5 +4,7 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('select').formSelect();
+    $(".dropdown-trigger").dropdown({ hover: false });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
